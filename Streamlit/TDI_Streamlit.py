3fbb7e73107d3f9525e7e67a450db160b363ea2b
“[Amanda <[amandamancini81492@gmail.com]>
2021-09-06 16:51:49 -0400

    Create streamlit files
    Upload app code and requirements fole

---
+++ Streamlit/TDI_Streamlit.py
@@ -0,0 +1,138 @@
+import streamlit as st
+import numpy as np
+import requests
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def string_to_arrayN(row):
+    if isinstance(row['N'], str):
+        irrad = row['N'].replace('[', '').replace(']', '').split()
+        return np.array([float(x) for x in irrad])
+    else:
+        return row['N']
+
+def string_to_arrayS(row):
+    if isinstance(row['S'], str):
+        irrad = row['S'].replace('[', '').replace(']', '').split()
+        return np.array([float(x) for x in irrad])
+    else:
+        return row['S']
+
+def string_to_arrayW(row):
+    if isinstance(row['W'], str):
+        irrad = row['W'].replace('[', '').replace(']', '').split()
+        return np.array([float(x) for x in irrad])
+    else:
+        return row['W']
+
+def string_to_arrayE(row):
+    if isinstance(row['E'], str):
+        irrad = row['E'].replace('[', '').replace(']', '').split()
+        return np.array([float(x) for x in irrad])
+    else:
+        return row['E']
+
+def relative_light(data, season):
+    """ Returns one word descriptor of irradiance intensity and variation.
+    Intensity values different for year vs season bc of number of days averaged."""
+
+    data_mean = np.mean(data)
+    data_std = np.std(data)
+
+    if season == 'Year':
+        if data_mean < 1:
+            data_light = 'low'
+        elif data_mean >= 1 and data_mean < 2:
+            data_light = 'moderate'
+        elif data_mean >= 2:
+            data_light = 'strong'
+
+        if data_std < 0.1:
+            data_var = 'little'
+        elif data_std >= 0.1 and data_std < 0.6:
+            data_var = 'some'
+        elif data_std >= 0.6:
+            data_var = 'large'
+    else:
+        if data_mean < 2.5:
+            data_light = 'low'
+        elif data_mean >= 2.5 and data_mean < 5:
+            data_light = 'moderate'
+        elif data_mean >= 5:
+            data_light = 'strong'
+
+        if data_std < 0.5:
+            data_var = 'little'
+        elif data_std >= 0.5 and data_std < 1.0:
+            data_var = 'some'
+        elif data_std >= 1.0:
+            data_var = 'large'
+
+    return (data_light, data_var)
+
+def floor_number(floor):
+    """ Returns a short description for the floor number."""
+    
+    if floor == 'All':
+        description = 'For this building'
+    elif str(floor)[-1] == '1' and floor != 11:
+        description = f'On the {floor}st floor'
+    elif str(floor)[-1] == '2' and floor != 12:
+        description = f'On the {floor}nd floor'
+    elif str(floor)[-1] == '3' and floor != 13:
+        description = f'On the {floor}rd floor'
+    else:
+        description = f'On the {floor}th floor'
+    
+    return description
+
+def color_change(ax, season):
+    """ Change values of histogram plot to show a heat-map color change with increasing light
+    intensity. Intensity values different for year vs season bc of number of days averaged."""
+
+    colors = ('#0343df', '#fffe7a', '#e50000')
+    blend = sns.blend_palette(colors, n_colors=7, as_cmap=False, input='rgb')
+    if season == 'Year':
+        for p in ax.patches:
+            if  p.get_x() <= 0.5:
+                p.set_color(blend[0]) #blue
+            elif  p.get_x() > 0.5 and p.get_x() <= 1.0:
+                p.set_color(blend[1])
+            elif  p.get_x() > 1.0 and p.get_x() <= 1.5:
+                p.set_color(blend[2])
+            elif  p.get_x() > 1.5 and p.get_x() <= 2.0:
+                p.set_color(blend[3])
+            elif  p.get_x() > 2.0 and p.get_x() <= 2.5:
+                p.set_color(blend[4])  
+            elif  p.get_x() > 2.5 and p.get_x() <= 3.0:
+                p.set_color(blend[5])
+            elif  p.get_x() > 3.0:
+                p.set_color(blend[6])
+    else: 
+        for p in ax.patches:
+            if  p.get_x() <= 1.0:
+                p.set_color(blend[0]) #blue
+            elif  p.get_x() > 1.0 and p.get_x() <= 2.0:
+                p.set_color(blend[1])
+            elif  p.get_x() > 2.0 and p.get_x() <= 3.0:
+                p.set_color(blend[2])
+            elif  p.get_x() > 3.0 and p.get_x() <= 4.0:
+                p.set_color(blend[3])
+            elif  p.get_x() > 4.0 and p.get_x() <= 5.0:
+                p.set_color(blend[4])  
+            elif  p.get_x() > 5.0 and p.get_x() <= 6.0:
+                p.set_color(blend[5])
+            elif  p.get_x() > 6.0:
+                p.set_color(blend[6])
+
+def geocode(address):
+    params = {'format': 'json', 
+                'addressdetails': 1,
+                'q': address}
+
+    headers = {'user-agent': 'TDI'}   
+
+    r = requests.get('http://nominatim.openstreetmap.org/search', params=params, headers=headers).json()
+    return (float(r[0]['lat']), float(r[0]['lon']))
+
+    
---
+++ Streamlit/streamlit_app.py
@@ -0,0 +1,146 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import folium
+from streamlit_folium import folium_static
+import requests
+import sys
+from TDI_Streamlit import string_to_arrayN, string_to_arrayS, string_to_arrayW, string_to_arrayE, relative_light, floor_number, color_change, geocode
+    
+def app():
+    st.title('Understanding Natural Light in NYC')
+
+    st.markdown("""The amount of natural light you receive in NYC can change immensely across the year, so use this app to get a better understanding of the amount and variation of light you can expect to receive across the year and in different seasons.""")
+    st.markdown("""Enter an address to get information on natural light in your apartment building, office, etc.""")
+    st.markdown("""
+    - For now, this app only works for Manhattan addresses (other boroughs coming soon)
+    - Please format address as: 'NUMBER', 'STREET NAME', New York, NY 'ZIPCODE'
+    - Ground floor is considered to be floor #1""")
+    
+    st.sidebar.markdown("""**About this project:**""")
+    st.sidebar.markdown("""
+    - This app was created for tenants (and real estate owners) to better understand the amount of natural light they can expect to receive across the year on different floors of their building.
+    - Per-floor irradiance values were calculated assuming a ceiling height of 10ft, and are therefore unlikely to be accurate for commericial buildings that deviate from this value.
+    - Data for this app were downloaded from [NYCOpenData](https://opendata.cityofnewyork.us/) and the [NREL](https://nsrdb.nrel.gov/).
+    - For a full description of the project methods and access to code, check out my [GitHub] (https://github.com/amandamancini) repo.""")
+    
+    location = str(st.text_input('Enter your address here:', '285 Fulton St, New York, NY 10048'))
+    
+    location_upper = ' '.join(location.upper().replace('NEW YORK,', '')
+                         .replace('NEW YORK', '').replace('NY', '').split())
+    
+    address_df = pd.read_csv('/Users/amandamancini/Dropbox/TDI/Fellowship/Capstone/Data/Manhattan/manhattan_address.csv')
+    BIN = address_df[address_df['Full_Address'] == location_upper].iloc[0]['BIN']
+    
+    irradiance_df = pd.read_csv('/Users/amandamancini/Dropbox/TDI/Fellowship/Capstone/Data/Manhattan/sebe_results/irradiance_full.csv')
+    
+    @st.cache
+    def string_to_array(df):
+        """ Converts irradiances values from a string to an array. """
+        
+        df['N'] = df.apply(string_to_arrayN, axis=1)
+        df['S'] = df.apply(string_to_arrayS, axis=1)
+        df['W'] = df.apply(string_to_arrayW, axis=1)
+        df['E'] = df.apply(string_to_arrayE, axis=1)
+    
+        return df
+    
+    irradiance_df = string_to_array(irradiance_df)
+    
+    building = irradiance_df[irradiance_df['BIN'] == BIN]
+    
+    left_column, right_column = st.columns(2)
+    
+    with left_column:
+        season = st.selectbox('Choose a season:', ('Year', 'Winter', 'Spring', 'Summer', 'Autumn'))
+
+    floors = building['Floor'].unique()
+    with right_column:
+        floor = st.selectbox('Choose a floor:', tuple(['All'] + list(floors)))
+        
+    building_final = building[building['Season'] == season]
+    
+    if floor != 'All':
+        building_final = building_final[building_final['Floor'] == floor]
+    
+    dataN = np.concatenate(tuple(building_final['N']), axis=None)
+    dataS = np.concatenate(tuple(building_final['S']), axis=None)
+    dataW = np.concatenate(tuple(building_final['W']), axis=None)
+    dataE = np.concatenate(tuple(building_final['E']), axis=None)
+    
+    north_light, north_var = relative_light(dataN, season)
+    south_light, south_var = relative_light(dataS, season)
+    west_light, west_var = relative_light(dataW, season)
+    east_light, east_var = relative_light(dataE, season)
+    
+    description = floor_number(floor)
+        
+    if season == 'Year':
+        st.markdown(f'''{description} **across the year**:''')
+    elif season == 'Winter':
+        st.markdown(f'''{description} **in the winter**:''')
+    elif season == 'Spring':
+        st.markdown(f'''{description} **in the spring**:''')        
+    elif season == 'Summer':
+        st.markdown(f'''{description} **in the summer**:''')        
+    elif season == 'Autumn':
+        st.markdown(f'''{description} **in the autumn**:''')
+    
+    st.markdown(f'''
+    - You can execpt to receive {south_light} light for south-facing windows, with {south_var} variation across the southern wall. 
+    - East-facing windows should receive {east_light} light, with {east_var} variation across the eastern wall. 
+    - To the west, you will likely receive {west_light} light with {west_var} variation across the western wall. 
+    - Finally, you can execpt north-facing windows to recieve {north_light} light with {north_var} variation across the northern wall.''')
+    
+    if season == 'Year':
+        x_max = 3.5   
+    else:
+        x_max = 9.0        
+    
+    fig = plt.figure(figsize=(12, 12))
+    gs = fig.add_gridspec(2, 2)
+
+    with sns.axes_style("whitegrid"):
+        ax = fig.add_subplot(gs[0, 0])
+        sns.distplot(dataN, kde = False).set_title('North', y = 0.9, x=0.9, fontsize = 20)
+        ax.set(xlim=(0, x_max), xlabel='Daily Irradiance (kWh/$\mathregular{m^2}$/Day)', ylabel='Count of Wall Pixels (16$\mathregular{ft^2}$)')
+        color_change(ax, season)
+
+    with sns.axes_style("whitegrid"):
+        ax = fig.add_subplot(gs[0, 1])
+        sns.distplot(dataS, kde = False).set_title('South', y = 0.9, x=0.9, fontsize = 20)
+        ax.set(xlim=(0, x_max), xlabel='Daily Irradiance (kWh/$\mathregular{m^2}$/Day)', ylabel='Count of Wall Pixels (16$\mathregular{ft^2}$)')
+        color_change(ax, season)
+
+    with sns.axes_style("whitegrid"):
+        ax = fig.add_subplot(gs[1, 0])
+        sns.distplot(dataW, kde = False).set_title('West', y = 0.9, x=0.9, fontsize = 20)
+        ax.set(xlim=(0, x_max), xlabel='Daily Irradiance (kWh/$\mathregular{m^2}$/Day)', ylabel='Count of Wall Pixels (16$\mathregular{ft^2}$)')
+        color_change(ax, season)
+
+    with sns.axes_style("whitegrid"):
+        ax = fig.add_subplot(gs[1, 1])
+        sns.distplot(dataE, kde = False).set_title('East', y = 0.9, x=0.9, fontsize = 20)
+        ax.set(xlim=(0, x_max), xlabel='Daily Irradiance (kWh/$\mathregular{m^2}$/Day)', ylabel='Count of Wall Pixels (16$\mathregular{ft^2}$)')
+        color_change(ax, season)
+
+    fig.tight_layout()
+    st.pyplot(fig)
+
+    latlng = geocode(location)
+    
+    map_ = folium.Map(location=latlng, zoom_start=25)
+    folium.Marker(latlng, popup=folium.Popup(location, parse_html=True)).add_to(map_)
+    (folium.CircleMarker(latlng,
+                         popup=folium.Popup(location, parse_html=True),
+                         radius=5,
+                         color='#3186cc',
+                         fill_color='#3186cc')
+     .add_to(map_))
+    
+    folium_static(map_)
+    
+if __name__ == '__main__':
+    app()
