4dec9222797f21755ea99dc773fb9d87a6a56e12
“[Amanda <amandamancini81492@gmail.com>
2021-09-23 11:20:56 -0400

    Update TDI_Full_Irradiance.py
    correct work pipeline

--- Code/TDI_Full_Irradiance.py
+++ Code/TDI_Full_Irradiance.py
@@ -42,11 +42,9 @@ from processing_umep.processing_umep_provider import ProcessingUMEPProvider
 umep_provider = ProcessingUMEPProvider()
 QgsApplication.processingRegistry().addProvider(umep_provider)
 
-from TDI_Neighborhood import full_season_irradiances
+from TDI_Neighborhood import neighborhood_setup, full_season_irradiances
 
 folder = '/Users/amandamancini/Dropbox/TDI/Fellowship/Capstone/Data/Manhattan/'
-
-sebe_folder = os.path.join(f'{folder}', 'sebe_results/')
 
 from osgeo import ogr
 
@@ -62,19 +60,39 @@ for feature in layer:
     neighborhoods.append(feature.GetField('ntaname'))
 
 neighborhoods_folder = os.path.join(f'{folder}', 'neighborhoods/')
+if os.path.exists(neighborhoods_folder):
+    shutil.rmtree(neighborhoods_folder)
+
+os.mkdir(neighborhoods_folder)
+
+## neighborhood setup
+for neighborhood in neighborhoods[0:-1]: # dont do parks, etc
+    neighborhood_setup(neighborhood=neighborhood, DEM='Manhattan_DEM_HighComp', folder=folder)
+
+sebe_folder = os.path.join(f'{folder}', 'sebe_results/')
+if os.path.exists(sebe_folder):
+    shutil.rmtree(sebe_folder)
+
+os.mkdir(sebe_folder)
 
 ## yearly irradiance
+
 neighborhood_irradiances_year = {}
 
 for neighborhood in neighborhoods[0:-1]: # dont do parks, etc
     irrads = full_season_irradiances(neighborhood=neighborhood, meterological='PSMv3_2020_Year_UMEP', season = 'year', folder=folder)
     
     neighborhood_irradiances_year[f'{neighborhood}'] = irrads
-    
+
+    src_dir = os.path.join(f'{neighborhoods_folder}{neighborhood}/', 'year_sebe/')
     dest_dir = os.path.join(f'{sebe_folder}', f'{neighborhood}/year/')
+
+    shutil.copytree(src_dir, dest_dir)
 
     with open(f'{dest_dir}{neighborhood}_complete_building_irradiances_year.dill', 'wb') as f:
         dill.dump(irrads, f)
+
+    shutil.rmtree(src_dir)
 
 with open(f'{sebe_folder}Complete_building_irradiances_year.dill', 'wb') as f:
     dill.dump(neighborhood_irradiances_year, f)
@@ -87,10 +105,15 @@ for neighborhood in neighborhoods[0:-1]: # dont do parks, etc
     
     neighborhood_irradiances_winter[f'{neighborhood}'] = irrads
     
+    src_dir = os.path.join(f'{neighborhoods_folder}{neighborhood}/', 'winter_sebe/')
     dest_dir = os.path.join(f'{sebe_folder}', f'{neighborhood}/winter/')
+
+    shutil.copytree(src_dir, dest_dir)
 
     with open(f'{dest_dir}{neighborhood}_complete_building_irradiances_winter.dill', 'wb') as f:
         dill.dump(irrads, f)
+
+    shutil.rmtree(src_dir)
 
 with open(f'{sebe_folder}Complete_building_irradiances_winter.dill', 'wb') as f:
     dill.dump(neighborhood_irradiances_winter, f)
@@ -103,10 +126,15 @@ for neighborhood in neighborhoods[0:-1]: # dont do parks, etc
     
     neighborhood_irradiances_spring[f'{neighborhood}'] = irrads
     
+    src_dir = os.path.join(f'{neighborhoods_folder}{neighborhood}/', 'spring_sebe/')
     dest_dir = os.path.join(f'{sebe_folder}', f'{neighborhood}/spring/')
+
+    shutil.copytree(src_dir, dest_dir)
 
     with open(f'{dest_dir}{neighborhood}_complete_building_irradiances_spring.dill', 'wb') as f:
         dill.dump(irrads, f)
+
+    shutil.rmtree(src_dir)
 
 with open(f'{sebe_folder}Complete_building_irradiances_spring.dill', 'wb') as f:
     dill.dump(neighborhood_irradiances_spring, f)
@@ -119,10 +147,15 @@ for neighborhood in neighborhoods[0:-1]: # dont do parks, etc
     
     neighborhood_irradiances_summer[f'{neighborhood}'] = irrads
     
+    src_dir = os.path.join(f'{neighborhoods_folder}{neighborhood}/', 'summer_sebe/')
     dest_dir = os.path.join(f'{sebe_folder}', f'{neighborhood}/summer/')
+
+    shutil.copytree(src_dir, dest_dir)
 
     with open(f'{dest_dir}{neighborhood}_complete_building_irradiances_summer.dill', 'wb') as f:
         dill.dump(irrads, f)
+
+    shutil.rmtree(src_dir)
 
 with open(f'{sebe_folder}Complete_building_irradiances_summer.dill', 'wb') as f:
     dill.dump(neighborhood_irradiances_summer, f)
@@ -135,10 +168,15 @@ for neighborhood in neighborhoods[0:-1]: # dont do parks, etc
     
     neighborhood_irradiances_autumn[f'{neighborhood}'] = irrads
     
+    src_dir = os.path.join(f'{neighborhoods_folder}{neighborhood}/', 'autumn_sebe/')
     dest_dir = os.path.join(f'{sebe_folder}', f'{neighborhood}/autumn/')
+
+    shutil.copytree(src_dir, dest_dir)
 
     with open(f'{dest_dir}{neighborhood}_complete_building_irradiances_autumn.dill', 'wb') as f:
         dill.dump(irrads, f)
+
+    shutil.rmtree(src_dir)
 
 with open(f'{sebe_folder}Complete_building_irradiances_autumn.dill', 'wb') as f:
     dill.dump(neighborhood_irradiances_autumn, f)
